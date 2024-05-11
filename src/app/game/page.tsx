96b5dc50962762ee5e1b06d6ab39e648df0b66aa
Xiaochao Liu <xiaochao.liu@compass.com>
2024-05-12 00:00:04 +0800

    Refactor game

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,38 +2,48 @@
 
 import { useEffect, useMemo, useState } from "react";
 
-import { GraphemeCardProps } from "@/app/components/GraphemeCard";
-import { splitWord, WordCardProps } from "@/app/components/WordCard";
+import { GraphemeCard, GraphemeCardProps } from "@/app/components/GraphemeCard";
+import { splitWord, WordCard, WordCardProps } from "@/app/components/WordCard";
 import { PHONICS_LIST } from "@/app/constants/list";
-import useGraphemeBlindBox from "@/app/hooks/useGraphemeBlindBox";
 import useHowler from "@/app/hooks/useHowler";
 import useMeaning from "@/app/hooks/useMeaning";
 import useVoiceSelector from "@/app/hooks/useVoiceSelector";
-import useWordBlindBox from "@/app/hooks/useWordBlindBox";
 
 const Page = () => {
-  const [word, setWord] = useState<string | undefined>(undefined);
-  const [grapheme, setGrapheme] = useState<string | undefined>(undefined);
-  const [wordBoxIndex, setWordBoxIndex] = useState(0);
-  const [graphemeBoxIndex, setGraphemeBoxIndex] = useState(0);
   const [voice, setVoice] = useState<SpeechSynthesisVoice>();
+
+  const [selectedWord, setSelectedWord] = useState<string | undefined>(
+    undefined,
+  );
+  const [selectedGrapheme, setSelectedGrapheme] = useState<string | undefined>(
+    undefined,
+  );
+
+  const [selectedWordGroup, setSelectedWordGroup] = useState<
+    number | undefined
+  >(undefined);
+  const [selectedGraphemeGroup, setSelectedGraphemeGroup] = useState<
+    number | undefined
+  >(undefined);
+
+  const [wordGroupIndex, setWordGroupIndex] = useState([0, 0, 0, 0]);
+  const [graphemeGroupIndex, setGraphemeGroupIndex] = useState([0, 0, 0, 0]);
 
   const { meaningContent, showMeaning } = useMeaning();
   const { play } = useHowler();
   useVoiceSelector(setVoice);
 
-  const { wordList, graphemeList, testDict } = useMemo(() => {
+  const { wordGroups, graphemeGroups } = useMemo(() => {
     const wordList: WordCardProps[] = [];
     const graphemeList: GraphemeCardProps[] = [];
-    const testDict: any = {};
     PHONICS_LIST.map(({ phoneme, grapheme, pronunciation, tips, words }) => {
-      const word = words[0];
+      const word = words[Math.floor(Math.random() * (words.length - 1))];
       wordList.push({
         voice,
         grapheme,
         word: word.word,
         onClick: () => {
-          setWord(word.word);
+          setSelectedWord(grapheme);
           showMeaning({
             word: word.word,
             pronunciation: word.pronunciation,
@@ -46,7 +56,7 @@ const Page = () => {
         grapheme,
         pronunciation,
         onClick: () => {
-          setGrapheme(phoneme + grapheme);
+          setSelectedGrapheme(grapheme);
           play(phoneme);
           showMeaning({
             wordList: [{ word: grapheme }],
@@ -54,216 +64,107 @@ const Page = () => {
           });
         },
       });
-      testDict[phoneme + grapheme] = word;
     });
-    return { wordList, graphemeList, testDict };
+
+    const wordGroups: WordCardProps[][] = [[], [], [], []];
+    const wordDistribution = distribute(wordList);
+    wordList.forEach((wordCard, index) => {
+      wordGroups[wordDistribution[index]].push({
+        ...wordCard,
+        onClick: () => {
+          wordCard.onClick();
+          setSelectedWordGroup(wordDistribution[index]);
+        },
+      });
+    });
+
+    const graphemeGroups: GraphemeCardProps[][] = [[], [], [], []];
+    const graphemeDistribution = distribute(graphemeList);
+    graphemeList.forEach((graphemeCard, index) => {
+      graphemeGroups[graphemeDistribution[index]].push({
+        ...graphemeCard,
+        onClick: () => {
+          graphemeCard.onClick();
+          setSelectedGraphemeGroup(graphemeDistribution[index]);
+        },
+      });
+    });
+
+    return { wordGroups, graphemeGroups };
   }, [voice]); // do not add `play`, `showMeaning`
 
-  const graphemeArr = useMemo(() => {
-    const array: GraphemeCardProps[][] = [[], [], [], [], []];
-    const n = Math.ceil(graphemeList.length / 5);
-    let sArray: number[] = [];
-    for (let i = 0; i < n; i++) {
-      sArray = [...sArray, ...shuffleArray()];
-    }
-    graphemeList.forEach((word, index) => {
-      array[sArray[index]].push(word);
-    });
-    return array;
-  }, [graphemeList]);
-
-  const wordArr = useMemo(() => {
-    const array: WordCardProps[][] = [[], [], [], [], []];
-    const n = Math.ceil(wordList.length / 5);
-    let sArray: number[] = [];
-    for (let i = 0; i < n; i++) {
-      sArray = [...sArray, ...shuffleArray()];
-    }
-    wordList.forEach((word, index) => {
-      array[sArray[index]].push(word);
-    });
-    return array;
-  }, [wordList]);
-
-  const {
-    GraphemeBlindBox: GraphemeBlindBox1,
-    setGraphemeIndex: setGraphemeIndex1,
-  } = useGraphemeBlindBox({
-    graphemeCardList: graphemeArr[0],
-    boxIndex: 0,
-    onClick: (boxIndex) => {
-      setGraphemeBoxIndex(boxIndex);
-    },
-  });
-  const {
-    GraphemeBlindBox: GraphemeBlindBox2,
-    setGraphemeIndex: setGraphemeIndex2,
-  } = useGraphemeBlindBox({
-    graphemeCardList: graphemeArr[1],
-    boxIndex: 1,
-    onClick: (boxIndex) => {
-      setGraphemeBoxIndex(boxIndex);
-    },
-  });
-  const {
-    GraphemeBlindBox: GraphemeBlindBox3,
-    setGraphemeIndex: setGraphemeIndex3,
-  } = useGraphemeBlindBox({
-    graphemeCardList: graphemeArr[2],
-    boxIndex: 2,
-    onClick: (boxIndex) => {
-      setGraphemeBoxIndex(boxIndex);
-    },
-  });
-  const {
-    GraphemeBlindBox: GraphemeBlindBox4,
-    setGraphemeIndex: setGraphemeIndex4,
-  } = useGraphemeBlindBox({
-    graphemeCardList: graphemeArr[3],
-    boxIndex: 3,
-    onClick: (boxIndex) => {
-      setGraphemeBoxIndex(boxIndex);
-    },
-  });
-  const {
-    GraphemeBlindBox: GraphemeBlindBox5,
-    setGraphemeIndex: setGraphemeIndex5,
-  } = useGraphemeBlindBox({
-    graphemeCardList: graphemeArr[4],
-    boxIndex: 4,
-    onClick: (boxIndex) => {
-      setGraphemeBoxIndex(boxIndex);
-    },
-  });
-
-  const setGraphemeIndexList = useMemo(
-    () => [
-      setGraphemeIndex1,
-      setGraphemeIndex2,
-      setGraphemeIndex3,
-      setGraphemeIndex4,
-      setGraphemeIndex5,
-    ],
-    [],
+  const graphemeList = useMemo(
+    () => graphemeGroups.map((list, index) => list[graphemeGroupIndex[index]]),
+    [graphemeGroups, graphemeGroupIndex],
   );
 
-  const { WordBlindBox: WordBlindBox1, setWordIndex: setWordIndex1 } =
-    useWordBlindBox({
-      wordCardList: wordArr[0],
-      boxIndex: 0,
-      onClick: (boxIndex) => {
-        setWordBoxIndex(boxIndex);
-      },
-    });
-  const { WordBlindBox: WordBlindBox2, setWordIndex: setWordIndex2 } =
-    useWordBlindBox({
-      wordCardList: wordArr[1],
-      boxIndex: 1,
-      onClick: (boxIndex) => {
-        setWordBoxIndex(boxIndex);
-      },
-    });
-  const { WordBlindBox: WordBlindBox3, setWordIndex: setWordIndex3 } =
-    useWordBlindBox({
-      wordCardList: wordArr[2],
-      boxIndex: 2,
-      onClick: (boxIndex) => {
-        setWordBoxIndex(boxIndex);
-      },
-    });
-  const { WordBlindBox: WordBlindBox4, setWordIndex: setWordIndex4 } =
-    useWordBlindBox({
-      wordCardList: wordArr[3],
-      boxIndex: 3,
-      onClick: (boxIndex) => {
-        setWordBoxIndex(boxIndex);
-      },
-    });
-  const { WordBlindBox: WordBlindBox5, setWordIndex: setWordIndex5 } =
-    useWordBlindBox({
-      wordCardList: wordArr[4],
-      boxIndex: 4,
-      onClick: (boxIndex) => {
-        setWordBoxIndex(boxIndex);
-      },
-    });
-
-  const setWordIndexList = useMemo(
-    () => [
-      setWordIndex1,
-      setWordIndex2,
-      setWordIndex3,
-      setWordIndex4,
-      setWordIndex5,
-    ],
-    [],
+  const wordList = useMemo(
+    () => wordGroups.map((list, index) => list[wordGroupIndex[index]]),
+    [wordGroups, wordGroupIndex],
   );
 
   useEffect(() => {
-    console.log("testDict", testDict);
-    console.log("graphemeBoxIndex", graphemeBoxIndex);
-    console.log("wordBoxIndex", wordBoxIndex);
-    console.log("word", word);
-    console.log("grapheme", grapheme);
-    console.log("test", grapheme && testDict[grapheme].word === word);
-    if (word && grapheme && testDict[grapheme].word === word) {
-      setGraphemeIndexList[graphemeBoxIndex]((prev: number) =>
-        prev + 1 > graphemeArr[graphemeBoxIndex].length ? prev : prev + 1,
-      );
-      setWordIndexList[wordBoxIndex]((prev: number) =>
-        prev + 1 > wordArr[wordBoxIndex].length ? prev : prev + 1,
-      );
-      setWord(undefined);
-      setGrapheme(undefined);
+    if (
+      selectedWord !== undefined &&
+      selectedGrapheme !== undefined &&
+      selectedGrapheme === selectedWord
+    ) {
+      setGraphemeGroupIndex((prev) => {
+        if (
+          graphemeGroups[selectedGraphemeGroup!].length - 1 ===
+          prev[selectedGraphemeGroup!]
+        ) {
+          return prev;
+        }
+        const copy = [...prev];
+        copy[selectedGraphemeGroup!] += 1;
+        return copy;
+      });
+      setWordGroupIndex((prev) => {
+        if (
+          wordGroups[selectedWordGroup!].length - 1 ===
+          prev[selectedWordGroup!]
+        ) {
+          return prev;
+        }
+        const copy = [...prev];
+        copy[selectedWordGroup!] += 1;
+        return copy;
+      });
+      setSelectedWord(undefined);
+      setSelectedGrapheme(undefined);
+      setSelectedWordGroup(undefined);
+      setSelectedGraphemeGroup(undefined);
     }
   }, [
-    grapheme,
-    graphemeArr,
-    graphemeBoxIndex,
-    setGraphemeIndexList,
-    setWordIndexList,
-    testDict,
-    word,
-    wordArr,
-    wordBoxIndex,
+    graphemeGroups,
+    selectedGrapheme,
+    selectedGraphemeGroup,
+    selectedWord,
+    selectedWordGroup,
+    wordGroups,
   ]);
 
   return (
     <>
-      <div>
-        <div className="flex flex-row">
-          <div className="basis-1/5">
-            <GraphemeBlindBox1 />
-          </div>
-          <div className="basis-1/5">
-            <GraphemeBlindBox2 />
-          </div>
-          <div className="basis-1/5">
-            <GraphemeBlindBox3 />
-          </div>
-          <div className="basis-1/5">
-            <GraphemeBlindBox4 />
-          </div>
-          <div className="basis-1/5">
-            <GraphemeBlindBox5 />
-          </div>
+      <div className="flex flex-row mx-auto w-1/3">
+        <div className="basis-1/2">
+          {graphemeList.map((props, index) => {
+            return (
+              <div key={"grapheme" + index} className="h-40 grid">
+                <GraphemeCard {...props} />
+              </div>
+            );
+          })}
         </div>
-        <div className="flex flex-row">
-          <div className="basis-1/5">
-            <WordBlindBox1 />
-          </div>
-          <div className="basis-1/5">
-            <WordBlindBox2 />
-          </div>
-          <div className="basis-1/5">
-            <WordBlindBox3 />
-          </div>
-          <div className="basis-1/5">
-            <WordBlindBox4 />
-          </div>
-          <div className="basis-1/5">
-            <WordBlindBox5 />
-          </div>
+        <div className="basis-1/2">
+          {wordList.map((props, index) => {
+            return (
+              <div key={"word" + index} className="h-40 grid">
+                <WordCard {...props} />
+              </div>
+            );
+          })}
         </div>
       </div>
       {meaningContent}
@@ -271,13 +172,14 @@ const Page = () => {
   );
 };
 
-const shuffleArray = () => {
-  const array = [0, 1, 2, 3, 4];
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+const shuffle = () => {
+  return [0, 1, 2, 3].sort(() => Math.random() - 0.5);
+};
+
+const distribute = (list: WordCardProps[] | GraphemeCardProps[]) => {
+  return Array(Math.ceil(list.length / 4))
+    .fill(0)
+    .flatMap(() => shuffle());
 };
 
 export default Page;
